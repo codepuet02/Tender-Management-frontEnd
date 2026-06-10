c9dd8f2dbdd79ad0e54d342c34ab80ac994fdc6f
Davison Andres <puentesdavison@gmail.com>
2026-06-10 18:28:31 -0500

    feat:implementar header para indentificar seccion correspondiente mediante la identificacion de la ruta

--- src/layouts/mainLayout.jsx
+++ src/layouts/mainLayout.jsx
@@ -1,13 +1,30 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../components/sidebar";
+import { useLocation } from "react-router-dom";
 
 function MainLayout() {
+  const { pathname } = useLocation();
+
+  const titleMap = {
+    "/": "Dashboard",
+    "/tenders": "Licitaciones",
+    "/clients": "Clientes",
+    "/providers": "Proveedores",
+    "/catalog": "Catalogo",
+    "/products": "Productos",
+  };
+
   return (
     <div className="flex flex-row bg-page">
       <div>
         <Sidebar />
       </div>
       <div className="border border-black flex-1">
+        <header className="h-16 bg-white flex items-center px-4">
+          <h1 className="text-2xl font-semibold text-title">
+            {titleMap[pathname]}
+          </h1>
+        </header>
         <Outlet />
       </div>
     </div>
